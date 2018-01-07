99420e8b745dc20e0cf763721f4517b308c2b3e5
Celine <cc972@cornell.edu>
2018-01-07 01:43:43 -0500

    Added enable/disable application in app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+"""
+Main Flask application which runs the website.
+"""
+
 from flask import Flask, render_template
 import os
 import sys
@@ -6,7 +10,13 @@ sys.setdefaultencoding("utf8")
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello_world(): 
+    """
+    Main function.
+
+    NOTE: To enable rush applications, let apply_display="block".
+    To disable, let apply_display="none".
+    """
     with open("data/places/employers.txt", "r") as employers, \
         open("data/places/schools.txt", "r") as schools, \
         open("data/places/teams.txt", "r") as teams, \
@@ -19,9 +29,10 @@ def hello_world():
                                teams=teams.readlines(),
                                eboard=eboard.readlines()[1:],
                                semester=semesterly.readlines()[1:],
-                               brothers=brothers.readlines()[1:]
+                               brothers=brothers.readlines()[1:],
+                               apply_display="block"
                                )
 
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
