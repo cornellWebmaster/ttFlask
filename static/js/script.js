b99820da393d1a29c897733b96b2a2acaa2cf077
Celine <cc972@cornell.edu>
2019-04-16 16:20:23 -0400

    Redirects to internal site on title double click

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,10 @@
-//  this.on("maxfilesexceeded", function(file) {
-//     this.removeAllFiles();
-//     this.addFile(file);
-//  });
-// this.on("error", function(file){if (!file.accepted) this.removeFile(file);});
+/**
+ * This is a file that will be imported in ALL templates.
+ * Any script here will be accessible on any website page.
+ */
+
+$('#thetatau').dblclick((event) => {
+  console.log('Double clicked!');
+  debugger;
+  window.location.href = "https://sites.google.com/cornell.edu/thetatau";
+});
